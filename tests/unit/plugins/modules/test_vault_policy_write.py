edde7f8a677ef5eb1f6c09cf63ab1e4aa15fac12
Mathijs Westerhof <mathijswesterhof@outlook.com>
2024-10-25 16:52:31 +0200

    Add skeleton files for list get write and delete policies Unit tests

---
+++ tests/unit/plugins/modules/test_vault_policy_write.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2024 Mathijs Westerhof (@mathijswesterhof)
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import pytest
+
+from .....plugins.modules import vault_policy_write
+from .....plugins.module_utils._hashi_vault_common import HashiVaultValueError
+
+
+hvac = pytest.importorskip('hvac')
+
+
+class TestModuleVaultPolicyWrite():
+    # happy flow
+    def test_vault_policy_write_empty_acl(self):
+        pass
+
+    def test_vault_policy_write_data_acl(self):
+        pass
+
+    def test_vault_policy_write_empty_rgp(self):
+        pass
+
+    def test_vault_policy_write_data_rgp(self):
+        pass
+
+    def test_vault_policy_write_empty_egp(self):
+        pass
+
+    def test_vault_policy_write_data_egp(self):
+        pass
+
+    # unhappy flow
+    def test_vault_policy_write_hvac_error(self):
+        pass
+
+    def test_vault_policy_write_auth_error(self):
+        pass
+
+    def test_vault_policy_write_access_error(self):
+        pass
+
+    def test_vault_policy_write_unknown_policy_type_error(self):
+        pass
