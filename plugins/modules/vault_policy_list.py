130567f0f9df45b1be5344fdb741da87322d8156
Mathijs Westerhof <mathijswesterhof@outlook.com>
2024-10-30 13:43:17 +0100

    Add first unit tests for list and add fist list implementation

--- plugins/modules/vault_policy_list.py
+++ plugins/modules/vault_policy_list.py
@@ -24,15 +24,60 @@ seealso:
     link: https://developer.hashicorp.com/vault/docs/concepts/policies
 extends_documentation_fragment:
 options:
+  type:
+    description: Get a list of policies defined in Hashicorp Vault
+    type: str
+    default: acl
+    choices:
+      - acl
+      - rgp
+      - egp
 '''
 
 EXAMPLES = r'''
+- name: 'Get a list of acl policies'
+  community.hashi_vault.vault_policy_list:
+    type: acl
 '''
 
 RETURN = r'''
 raw:
+  description: The raw result of the read against the given path.
+  returned: success
+  type: dict
+  sample:
+    auth: null
+    data:
+      policies:
+        - policy_a
+        - policy_b
+        - default
+        - root
+    lease_duration: 2764800
+    lease_id: ""
+    renewable: false
+    request_id: e99f145f-f02a-7073-1229-e3f191057a70
+    warnings: null
+    wrap_info: null
 data:
+  description: The C(data) field of raw result. This can also be accessed via C(raw.data).
+  returned: success
+  type: dict
+  sample:
+    policies:
+      - policy_a
+      - policy_b
+      - default
+      - root
 policies:
+  description: The C(data.policies) field of the raw result. This is the list variant of C(data) in the return values.
+  returned: success
+  type: list
+  sample:
+    - policy_a
+    - policy_b
+    - default
+    - root
 '''
 
 import traceback
@@ -45,6 +90,7 @@ from ansible_collections.community.hashi_vault.plugins.module_utils._hashi_vault
 
 def run_module():
     argspec = HashiVaultModule.generate_argspec(
+        type=dict(type='str', default='acl', choices=['acl', 'rgp', 'egp']),
     )
 
     module = HashiVaultModule(
@@ -52,7 +98,29 @@ def run_module():
         supports_check_mode=True
     )
 
-    module.exit_json()
+    policy_type = module.params.get('type')
+    path = f'sys/policies/{policy_type}'
+    # Do note! there is a difference in output between sys/policy and sys/policies/acl where the first returns duplicate data under various keys
+
+    module.connection_options.process_connection_options()
+    client_args = module.connection_options.get_hvac_connection_options()
+    client = module.helper.get_vault_client(**client_args)
+    hvac_exceptions = module.helper.get_hvac().exceptions
+
+    try:
+        module.authenticator.validate()
+        module.authenticator.authenticate(client)
+    except (NotImplementedError, HashiVaultValueError) as e:
+        module.fail_json(msg=to_native(e), exception=traceback.format_exc())
+
+    try:
+        raw = client.list(path)
+    except hvac_exceptions.Forbidden as e:
+        module.fail_json(msg="Forbidden: Permission Denied to path ['%s']." % path, exception=traceback.format_exc())
+
+    data = raw['data']
+    policies = data['keys']
+    module.exit_json(raw=raw, data=data, policies=policies)
 
 
 def main():
--- tests/unit/plugins/modules/test_vault_policy_list.py
+++ tests/unit/plugins/modules/test_vault_policy_list.py
@@ -15,13 +15,52 @@ from .....plugins.module_utils._hashi_vault_common import HashiVaultValueError
 hvac = pytest.importorskip('hvac')
 
 
+def _connection_options():
+    return {
+        'auth_method': 'token',
+        'url': 'http://myvault',
+        'token': 'beep-boop',
+    }
+
+
+def _combined_options(**kwargs):
+    opt = _connection_options()
+    opt.update(kwargs)
+    return opt
+
+LIST_FIXTURES = [
+    'policy_list_response.json',
+]
+
+
+@pytest.fixture(params=LIST_FIXTURES)
+def list_response(request, fixture_loader):
+    return fixture_loader(request.param)
+
+
 class TestModuleVaultPolicyList():
     # happy flow
     def test_vault_policy_list_get_return_empty_acl(self):
         pass
 
-    def test_vault_policy_list_get_return_data_acl(self):
-        pass
+    @pytest.mark.parametrize('patch_ansible_module', [_combined_options(type='acl')], indirect=True)
+    def test_vault_policy_list_get_return_data_acl(self, patch_ansible_module, list_response, vault_client, capfd):
+        client = vault_client
+        client.list.return_value = list_response.copy()
+
+        with pytest.raises(SystemExit) as e:
+            vault_policy_list.main()
+
+        out, err = capfd.readouterr()
+        result = json.loads(out)
+
+        assert e.value.code == 0, "result: %r" % (result,)
+
+        client.list.assert_called_once_with(patch_ansible_module['path'])
+
+        assert result[
+                   'data'] == list_response, "module result did not match expected result:\nexpected: %r\ngot: %r" % (
+        list_response, result)
 
     def test_vault_policy_list_get_return_empty_rgp(self):
         pass
